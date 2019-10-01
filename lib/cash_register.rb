3512c222b1074fd2812e5296a887b472b83e1462
Adam Weissman <adam@comedytragedyepic.com>
2019-10-01 08:51:55 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,12 +1,12 @@
 class CashRegister
-  attr_accessor :total, :discount
 
-  def initialize(total=0)
-    @total = total
-  end
-  
-  def discount(discount)
+  attr_accessor :total, :discount, :items, :last_transaction
+
+  def initialize(discount=0)
+    @total = 0
     @discount = discount
+    @items = []
+  end
 
   def add_item(item, price)
   end
