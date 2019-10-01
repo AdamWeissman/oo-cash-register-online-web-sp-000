58a23ed46806718f4d42fc1e53540b566861f8b6
Adam Weissman <adam@comedytragedyepic.com>
2019-10-01 09:12:34 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -13,6 +13,7 @@ class CashRegister
     quantity.times do
       items << title
     end
+    @last_transaction = (price * quantity)
   end
 
   def apply_discount
@@ -26,6 +27,7 @@ class CashRegister
   end
 
   def void_last_transaction
+    @total -= @last_transaction
   end
 
 
